45c84394185067b2ee2ab655108657684c599eb7
Lutfiah-s <lutfiah.sahira23@sma.belajar.id>
2025-06-12 16:26:13 +0700

    fix: update config.php with Railway MySQL settings

--- config.php
+++ config.php
@@ -1,13 +1,14 @@
 <?php
-// config.php
-$host = 'localhost';
-$user = 'root';
-$pass = '';
-$db   = 'photvite';
+$host = 'containers-us-west-200.railway.app'; // host database dari Railway
+$user = 'root'; // user default
+$password = 'ctLonqvoZCUnJQ'; // password dari Railway
+$database = 'photvite'; // nama database kamu
+$port = 3306; // port dari Railway
 
-$conn = new mysqli($host, $user, $pass, $db);
+$conn = new mysqli($host, $user, $password, $database, $port);
 
+// Cek koneksi
 if ($conn->connect_error) {
-  die("Koneksi gagal: " . $conn->connect_error);
+    die("Koneksi gagal: " . $conn->connect_error);
 }
 ?>
